9d90d60ce60a8ab1c2d215b6353f6fc3dac0330d
MikeFogz <MikeFogz@gmail.com>
2020-11-05 20:46:43 -0800

    modified app.js and removed api project fold duplicate

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-fetch('api.openweathermap.org/data/2.5/weather?zip=94611,us&appid={d6023dc0b7605243dca140129a0bd39b}')
+fetch('https://data.covidactnow.org/latest/us/counties/06001.NO_INTERVENTION.timeseries.json')
   .then(response => response.json())
   .then(data => console.log(data));
 
-  api.openweathermap.org/data/2.5/weather?zip=94040,us&appid={d6023dc0b7605243dca140129a0bd39b}
